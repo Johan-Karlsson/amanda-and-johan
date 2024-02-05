4114eb174df71879d5940f4e70fd2cb20c2ba2cf
Johan-Karlsson <johan_951111@hotmail.com>
2024-02-05 22:19:22 +0100

    Updated the welcome paragraph with more text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ export default function MyApp() {
   return (
     <div>
       <h1>Välkomna till vår bröllopshemsida!</h1>
-      <p>Jag jobbar just nu på att komma igång med byggandet av hemsida, håll till godo!</p>
+      <p>Jag jobbar just nu på att komma igång med byggandet av hemsidan, håll till godo! Det går lite sådär faktiskt.</p>
     </div>
   );
 }
