93f8ff11e9b19c7b009347215fe733db3536d29c
Johan-Karlsson <johan_951111@hotmail.com>
2024-02-10 18:44:09 +0100

    Added bootstrap, changed styling and configured naming + favicon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
 export default function MyApp() {
   return (
-    <div>
+    <div id="app">
       <h1>Välkomna till vår bröllopshemsida!</h1>
       <p>Jag jobbar just nu på att komma igång med byggandet av hemsidan, håll till godo! Fler uppdateringar kommer under våren.</p>
     </div>
